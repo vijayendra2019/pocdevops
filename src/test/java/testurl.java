4e74d819ad39587e9354cf8b23e18f9ab6c16ee1
Vijayendra Sandeep.v <vijayendrav@pyramid.com>
2021-09-06 22:02:27 +0530

    Test Commit

---
+++ src/test/java/testurl.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class testurl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		System.setProperty("Webdriver.ie.driver", ".\\IEDriverServer.exe");
+//		WebDriver driver=new InternetExplorerDriver();  
+//		/*System.setProperty("Webdriver.chromedriver.driver", ".\\chromedriver.exe");
+		WebDriver driver  = new ChromeDriver();
+//		//System.out.println(URL);*/
+//		driver.navigate().to("https://demoqa.com/automation-practice-form");
+		
+		
+	
+	}
+
+}
